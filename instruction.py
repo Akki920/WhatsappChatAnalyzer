f82236331f91d3446a0042bad766109fee958b74
Akshit Joshi <akshit.joshi920@gmail.com>
2024-05-17 10:54:40 +0530

    add one instruction

--- instruction.py
+++ instruction.py
@@ -7,6 +7,7 @@ def content():
     st.write("1. open the chat")
     st.write("2. Tap More option")
     st.write("3. Tap on 'Without Media' (will bring more support soon for with media)")
+    st.header("Please make sure that your whatsapp have 12-hour time format (We'll bring 24-hour time format support soon)")
 
     st.image("Images/guide_to_export_chat.jpg", caption="How to export chat")
 
